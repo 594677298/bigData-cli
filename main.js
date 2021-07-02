255e4496bba009e0fd75c70afd89dfebf45738c4
zhangzhongshan <594677298@qq.com>
2021-07-02 17:40:25 +0800

    init

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 #! /usr/bin/env node
 
 const cmd = require('commander') // 交互
-const chalk = require('chalk')// 颜色
+const chalk = require('chalk') // 颜色
 const downGit = require('./src/downLoad') // 下载
 const options = require('./src/options')
+const getGit = require('./src/bigdata-cli')
 
 cmd.command('init').description('初始化模板').action(async (args) => {
-  typeof args !== 'string' && (console.log(chalk.red('缺少必填参数')), process.exit(1))
+  if (typeof args !== 'string') {
+    console.log(chalk.red('缺少必填参数'))
+    process.exit(1)
+  }
   console.log(chalk.yellow('bjgData脚手架初始化模板\n'))
   // 填选项
   await options()
   // 拉取
-  require('./src/bigdata-cli')()
+  await getGit()
   await downGit(args)
 })
 cmd.parse(process.argv)
